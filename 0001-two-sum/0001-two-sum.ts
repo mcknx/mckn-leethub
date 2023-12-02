0933388ac2cc3e3599b156716718c82846f765c1
mcknx <50982785+mcknx@users.noreply.github.com>
2023-12-02 14:47:52 +0800

    Time: 56 ms (87.60%), Space: 45.1 MB (45.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,20 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
-    const numIndicesMap: Record<number, number> = {};
+    const numIndices: Record<number, number>= {}
 
     for (let i = 0; i < nums.length; i++) {
-        const currentNum = nums[i];
-        const complement = target - currentNum;
+        const currentNum = nums[i]
+        const complement = target - currentNum
 
-        // Check if the complement exists in the map
-        if (numIndicesMap.hasOwnProperty(complement)) {
-            // Return the indices if found
-            return [numIndicesMap[complement], i];
+        if (numIndices.hasOwnProperty(complement)) {
+            return [numIndices[complement], i]
         }
 
-        // Store the current number and its index in the map
-        numIndicesMap[currentNum] = i;
+        numIndices[currentNum] = i
     }
 
-    // Return an empty array if no solution is found
+    // base case
     return [];
 }
